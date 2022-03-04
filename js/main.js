39543e6d3f94e13845b2843411054cacaa3eb3d8
Maksim <zawadzki2778@gmail.com>
2022-03-05 01:39:00 +0300

    устранил замечания наставника

--- js/main.js
+++ js/main.js
@@ -8,10 +8,10 @@ function getRandomIntInclusive(min, max) {
 }
 getRandomIntInclusive();
 
-// function checkLengthComment(str, maxlength) {
-//   return str.length <= maxlength;
-// }
-// checkLengthComment();
+function checkLengthComment(str, maxlength) {
+  return str.length <= maxlength;
+}
+checkLengthComment('str', 'maxlength');
 
 const MESSAGE = [
   'Всё отлично!',
@@ -40,16 +40,23 @@ const DESCRIPTION = [
 
 const userId = Array.from({length: 25}, (v, i) => ++i);
 const userUrl = Array.from({length: 25}, (v, i) => ++i);
+const commentsId = Array.from({length: 1000}, (v, i) => ++i);
 
 const getUniqueNumber = (array) => array.splice(getRandomIntInclusive(0, (array.length - 1)), 1);
 
-const createObject = () => ({
-  id: getUniqueNumber(userId)[0],
-  url: `photos/${getUniqueNumber(userUrl)}.png`,
+const createCommetsObject = () => ({
+  id: getUniqueNumber(commentsId)[0],
   avatar: `img/-${getRandomIntInclusive(1, 6)}.svg`,
-  likes: getRandomIntInclusive(15, 200),
-  description: DESCRIPTION[getRandomIntInclusive(0, DESCRIPTION.length - 1)],
   message: MESSAGE[getRandomIntInclusive(0, MESSAGE.length - 1)],
   name: NAME[getRandomIntInclusive(0, NAME.length - 1)]
 });
-const comments = Array.from({length: 25}, createObject);
+
+const createPhoto = () => ({
+  id: getUniqueNumber(userId)[0],
+  url: `photos/${getUniqueNumber(userUrl)}.png`,
+  likes: getRandomIntInclusive(15, 200),
+  description: DESCRIPTION[getRandomIntInclusive(0, DESCRIPTION.length - 1)],
+  comments: Array.from({length: getRandomIntInclusive(1, 10)}, createCommetsObject)
+});
+// eslint-disable-next-line no-unused-vars
+const photosMock = Array.from({length: 25}, createPhoto);
